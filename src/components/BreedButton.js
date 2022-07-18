d2fcffa45878a1b57e0f0f6f7bd93d756eb5d9b8
rom-ero <roman.s@halfhelix.com>
2022-07-18 09:46:54 +0200

    button fix

--- src/components/BreedButton.js
+++ src/components/BreedButton.js
@@ -4,6 +4,6 @@ export default function BreedButton({ content: breed, onClick }) {
     const { label } = breed
 
     return (
-        <a className="button button--gray button--effect  margin-10" onClick={() => onClick(breed)} key={label}><span>{label}</span></a>
+        <button className="button button--gray button--effect  margin-10" onClick={() => onClick(breed)} key={label}><span>{label}</span></button>
     )
 }
